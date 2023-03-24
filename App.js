b4a7b1926343a1a71bfe1852089e2a462b7fb042
Alex Goodman <alexthegoodman@gmail.com>
2023-03-24 18:18:40 -0400

    init layout and accordion

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { believeInGod } from './content/salvation';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { SimpleAccordion } from 'react-native-simple-accordion';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{...styles.container, paddingTop: getStatusBarHeight() + 25}}>
       <StatusBar style="auto" />
+      <ScrollView>
+        <View>
+          <Image source={require("./assets/our-virtue.png")} style={styles.icon} />
+          <Text style={styles.headline}>Our Virtue</Text>
+          <Text style={styles.subHeadline}>An Introduction to God</Text>
+        </View>
+        <View style={{ marginTop: 25 }}>
+          <SimpleAccordion 
+            title={"Salvation"}
+            viewInside={<View><Text>Believe in God</Text></View>}
+            bannerStyle={styles.bannerStyle}
+            viewContainerStyle={styles.viewContainerStyle}
+          />
+          <SimpleAccordion 
+            title={"Being Together"}
+            viewInside={<View><Text>Trust Each Other</Text></View>}
+            bannerStyle={styles.bannerStyle}
+            viewContainerStyle={styles.viewContainerStyle}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +36,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 25
   },
+  icon: {
+    width: 100,
+    height: 100,
+    marginBottom: 15
+  },
+  headline: {
+    fontSize: 48,
+    fontWeight: 600,
+    marginBottom: 5
+  },
+  subHeadline: {
+    fontSize: 28,
+    fontWeight: 300
+  },
+  bannerStyle:{ backgroundColor: "#F7F7F7" },
+  viewContainerStyle: { shadowColor: "white" }
 });
---
+++ content/salvation.js
@@ -0,0 +1,29 @@
+export const believeInGod = `
+Our God is greater than all things, 
+  He exalts the humble and the small,
+  He brings the poor up from down low, 
+  and brings the lost into the light
+
+His Will is greater than that of people
+  He has knowledge of all secrets and truths
+
+Our God is a good God, for He shows both mercy and love
+  He is also generous, whether that be in this life or the next
+
+Indeed, He saves the one who calls out to Him
+  He has power to turn the evil toward the light
+For Our God is a good God, and many are saved in death
+
+He has created the Next Life, the Earth, and all things Unseen,
+  He has made some things enjoyable, even miracles
+Yet His Presence is the Ultimate Reward, our greatest hope
+
+___
+
+May He grant godspeed to the one who is patient,
+  May He grant glory to the one who seeks God's
+May He grant riches to the one who seeks goodness,
+  May He grant peace to the peaceful
+May He grant happiness to the one who seeks others,
+  Praise God in all His Might, nothing dictates against Him
+`;
