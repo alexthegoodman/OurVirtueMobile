d7a16c8649cfc9adf030cb9296c19604751e9a39
Alex Goodman <alexthegoodman@gmail.com>
2023-04-07 16:11:28 -0400

    rest of english

--- content/theStraightPath/useModeration.js
+++ content/theStraightPath/useModeration.js
@@ -1,4 +1,16 @@
-export const useModerationEn = ``;
+export const useModerationEn = `
+Extremes are traps for the unwise
+    Counter-productive and unreasonable
+Yet so attractive to the unaided eye
+
+It's better to leave nothing overdone or underdone
+    Be both mindful and moderate
+And seek what is most appropriate
+
+Our Father delights in those who care for each other
+    So do not seek after anything dangerous
+    And surely, extremes are dangerous
+`;
 
 export const useModerationHi = ``;
 
