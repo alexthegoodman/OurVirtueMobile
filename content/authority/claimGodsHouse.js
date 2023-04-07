95d59d5eaeb7d8a34d585feec8ee8aa8e6917b22
Alex Goodman <alexthegoodman@gmail.com>
2023-04-06 20:00:12 -0400

    authority english

--- content/authority/claimGodsHouse.js
+++ content/authority/claimGodsHouse.js
@@ -1,4 +1,17 @@
-export const claimGodsHouseEn = ``;
+export const claimGodsHouseEn = `
+The owner of the house
+    Carries the blame and its burdens
+But a homeowner should be reliable
+
+Leaders are disgusted by neglect
+    They care for their people dearly
+And ensure their protection
+
+Let them fill their house with light
+    Calling their Creator to be the Head
+Declaring their house God's House
+    A house abiding by God's Commandments
+`;
 
 export const claimGodsHouseHi = ``;
 
