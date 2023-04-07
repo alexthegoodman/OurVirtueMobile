eb766561846cb20629518015ac92f83127ae553e
Alex Goodman <alexthegoodman@gmail.com>
2023-04-06 20:12:35 -0400

    his design english

--- content/hisDesign/chooseFreely.js
+++ content/hisDesign/chooseFreely.js
@@ -1,4 +1,12 @@
-export const chooseFreelyEn = ``;
+export const chooseFreelyEn = `
+Our Father created each person
+    Each one an individual
+He gave us minds and hearts of our own
+    To make the choice between right and wrong
+Indeed, He provides for you a finite environment
+    Within which He provides space to choose
+From which He shall judge your heart
+`;
 
 export const chooseFreelyHi = ``;
 
